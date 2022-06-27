fa05205a90bd3497be9ca170ef0afac38e5263a3
bva <hochochoc999@gmail.com>
2022-06-27 23:58:42 +0700

    add Header component

---
+++ DIProject/frontend/src/router/index.js
@@ -0,0 +1,20 @@
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
+//
+// Vue.use(VueRouter);
+//
+// export default new Router({
+//     routes: [
+//         // {
+//         //     path: '/',
+//         //     name: 'Home',
+//         //     component: Home,
+//         //     children: [
+//         //         {
+//         //             path: '/',
+//         //             component: index
+//         //         }
+//         //     ]
+//         // }
+//     ]
+// })
